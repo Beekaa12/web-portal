21211a97fefacba2120882c8fc0ca731f8e50d6c
Beekaa12 <tuchoyadeta@gmail.com>
2026-01-19 17:53:22 +0300

    Fix

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -23,11 +23,14 @@ const Contact = () => {
     setError("");
 
     try {
-      const response = await fetch("http://localhost:5000/api/feedback", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(formData),
-      });
+      const response = await fetch(
+        `${import.meta.env.VITE_API_URL}/api/feedback`,
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(formData),
+        },
+      );
 
       if (!response.ok) {
         const data = await response.json();
